744ee049458a2853fa4971556749e18a8a48c7c5
sankalp7654 <sankalp.saxena.sta@gmail.com>
2018-10-04 03:41:54 +0530

    initial commit

---
+++ src/com/java/sankalp/Socket/Client.java
@@ -0,0 +1,49 @@
+package com.java.sankalp.Socket;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+class Client {
+
+	public static void main(String ... args) throws Exception{
+		
+		String ip = "localhost";
+		int port = 6969;
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			Socket socket = new Socket(ip, port);
+			
+			System.out.println("Enter your name");
+			String string = scanner.nextLine();
+			
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(string);
+            
+          
+            	   System.out.println("Chat with the Server" + "\n");
+                   
+                   System.out.print("You: ");
+                   String message = scanner.nextLine();
+                   out.println(message);
+                   
+                   BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                   while(true) { 
+                   if(!input.ready()) {
+                       System.out.println(input.readLine() + "\n");
+                       
+                       System.out.print("You: ");
+                       String sendMessage = scanner.nextLine();
+                       out.println(sendMessage);
+	                   	
+                     	       				                     
+       			}
+                   
+            }
+         
+	}
+}
---
+++ src/com/java/sankalp/Socket/Server.java
@@ -0,0 +1,51 @@
+package com.java.sankalp.Socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+	public class Server {
+	    
+		public static void main(String ...strings) throws IOException {
+	
+			ServerSocket serverSocket = new ServerSocket(6969);
+			System.out.println("Server is up and running");
+				Socket socket = serverSocket.accept();
+				Scanner scanner = new Scanner(System.in);
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String clientName = br.readLine();
+				System.out.println(clientName + " connected");
+				System.out.println("---------------------------------------------");
+
+				System.out.println();
+				
+			while(true) {
+	
+				if(!br.ready()) {
+					String getMessage = br.readLine();
+					System.out.println(clientName + ": " + getMessage);
+					String message = "Server: ";
+					System.out.print(message);
+					message += scanner.nextLine();
+					System.out.println();
+					
+		
+					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	                out.println(message);
+	              
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	
